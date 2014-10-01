e9b6e9941edb33210d6c3150daac2fd33b443674
Matt Rubel <mrubel92@gmail.com>
2014-10-01 00:08:47 -0400

    Added overloaded methods to fold in Fold class

---
+++ fold.py
@@ -0,0 +1,21 @@
+from fold_math import add
+from fold_math import subtract
+from fold_math import divide
+from fold_math import multiply
+
+class Fold (object):
+
+	def __init__(self, core):
+		self.cores = core
+		
+	def __add__(self):
+		return add([18,31,27,2], self.cores)
+
+	def __truediv__(self):
+		return divide([72,3,4], self.cores)
+
+	def __sub__(self):
+		return subtract([14,3,7,2], self.cores)
+
+	def __mult__(self):
+		return multiply([3,2,8,10], self.cores)
